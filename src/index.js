801c38dcd5e63e36a96088a6f14a24c572b1c556
koichi-sann <vasilyev62@gmail.com>
2022-01-31 02:16:04 +0300

    feat: add JS: brackets solution?

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const stack = []
+  const splitStr = str.split('')
+  stack.push(splitStr[0])
+  for (let i = 1; i < splitStr.length; i++) {
+    stack.push(splitStr[i])
+    for (let j = 0; j < bracketsConfig.length; j++) {
+      if (
+        splitStr[i] == bracketsConfig[j][1] &&
+        stack[stack.length - 2] == bracketsConfig[j][0]
+      ) {
+        stack.pop()
+        stack.pop()
+      }
+    }
+  }
+
+  if (stack.length == 0) return true
+  return false
 }
